ed35bb2a1eef107b7d752625e3ddb6dccb75b6d1
whitewalker007 <nitinkrshukla@gmail.com>
2017-06-19 17:00:07 +0530

    Correct one

--- code/Gen_lmdb.py
+++ code/Gen_lmdb.py
@@ -4,8 +4,8 @@ Description     :It divides the training images into 2 sets and stores them in l
 Author          :Nitin Shukla
 Date Created    :20170616
 '''
-import glob
 import os
+import glob
 import random
 import numpy as np
 
@@ -49,8 +49,8 @@ os.system('rm -rf  ' + train_lmdb)
 os.system('rm -rf  ' + validation_lmdb)
 
 
-df_train = [img for img in glob.glob("../input/train/*jpg")]
-df_test = [img for img in glob.glob("../input/test1/*jpg")]
+df_train = [img for img in glob.glob("/home/ubuntu/DeepLearning_crop_classification/input/train/*jpg")]
+df_test = [img for img in glob.glob("/home/ubuntu/DeepLearning_crop_classification/input/test/*jpg")]
 
 #We should shuffle df_train
 random.shuffle(df_train)
@@ -60,22 +60,22 @@ print 'Generating train_lmdb'
 in_db = lmdb.open(train_lmdb, map_size=int(1e12))
 with in_db.begin(write=True) as in_txn:
     for in_idx, img_path in enumerate(df_train):
-        if in_idx %  6!= 0:
+        if in_idx %  6 == 0:
             continue
         img = cv2.imread(img_path, cv2.IMREAD_COLOR)
         img = transform_img(img, img_width=IMG_WIDTH, img_height=IMG_HEIGHT)
-        if 'barley' in img_path:
+        if 'wheat' in img_path:
             label=0
-        # elif 'barley' in img_path:
-        #     label=1
-        # elif 'rice' in img_path:
-        #     label=2
-        # elif 'soybean' in img_path:
-        #     label=3
-        # elif 'millets' in img_path:
-        #     label=4
+        elif 'barley' in img_path:
+            label=1
+        elif 'rice' in img_path:
+            label=2
+        elif 'soyabean' in img_path:
+            label=3
+        elif 'millets' in img_path:
+            label=4
         else:
-            label=1   #Sunflower
+            label=5   #Sunflower
         datum = make_datum(img, label)
         in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
         print '{:0>5d}'.format(in_idx) + ':' + img_path
@@ -87,22 +87,22 @@ print '\nGenerating validation_lmdb'
 in_db = lmdb.open(validation_lmdb, map_size=int(1e12))
 with in_db.begin(write=True) as in_txn:
     for in_idx, img_path in enumerate(df_train):
-        if in_idx % 6 == 0:
+        if in_idx % 6 != 0:
             continue
         img = cv2.imread(img_path, cv2.IMREAD_COLOR)
         img = transform_img(img, img_width=IMG_WIDTH, img_height=IMG_HEIGHT)
-        if 'barley' in img_path:
+        if 'wheat' in img_path:
             label=0
-        # elif 'barley' in img_path:
-        #     label=1
-        # elif 'rice' in img_path:
-        #     label=2
-        # elif 'soybean' in img_path:
-        #     label=3
-        # elif 'millets' in img_path:
-        #     label=4
+        elif 'barley' in img_path:
+            label=1
+        elif 'rice' in img_path:
+            label=2
+        elif 'soyabean' in img_path:
+            label=3
+        elif 'millets' in img_path:
+            label=4
         else:
-            label=1    #Sunflower
+            label=5    #Sunflower
         datum = make_datum(img, label)
         in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
         print '{:0>5d}'.format(in_idx) + ':' + img_path
